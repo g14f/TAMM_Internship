9efbedff7fa1578f4f13efe59a17f64cc4e30dfe
g14f <yousef.abdulrahman8@gmail.com>
2025-07-12 19:19:50 +0400

    Update app.py

--- ChatWithData/app.py
+++ ChatWithData/app.py
@@ -74,7 +74,7 @@ if st.session_state.csv and st.session_state.description and st.session_state.js
         df = SmartDataframe(df, config={"llm": llm})
         source = Source(type="csv", path=st.session_state.path)
         schema = SemanticLayerSchema(name="schema1", description=st.session_state.description_text, columns=st.session_state.columns, source=source,dataframe=df)
-        st.write(df.columns)
+        print(df.columns)
         st.session_state.df = pai.DataFrame(data=df, schema=schema)
     st.session_state.csv = True
     st.session_state.description = True
